f79ddb418bb56371e11abe7ed8e4b64c32c5e235
MSabbirHossen <mshossen724@gmail.com>
2025-10-04 17:58:52 +0300

    Basic CSS Added

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import "./Country.css";
 
 const Country = ({ country }) => {
   // console.log(country);
   //   const { name } = country;
   return (
-    <div>
+    <div className="country">
       <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
       <h2>Name: {country.name.common}</h2>
       <p>Total Population: {country.population.population}</p>
--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,5 +1,6 @@
 import React, { use } from "react";
 import Country from "../Country/Country";
+import "./Countries.css";
 
 const Countries = ({ countriesPromise }) => {
   const countriesData = use(countriesPromise);
@@ -9,9 +10,11 @@ const Countries = ({ countriesPromise }) => {
   return (
     <>
       <h1>Here {countries.length} countries data available.</h1>
-      {countries.map((country) => (
-        <Country key={country.cca3.cca3} country={country}></Country>
-      ))}
+      <div className="countries">
+        {countries.map((country) => (
+          <Country key={country.cca3.cca3} country={country}></Country>
+        ))}
+      </div>
     </>
   );
 };
